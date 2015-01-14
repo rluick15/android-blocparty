05b96fe8fc45f9a5153ecdf9845e13935c0ab1ee
Richard Luick <rluick15@gmail.com>
2015-01-14 16:14:33 -0500

    finish onboarding flow with dismiss button

--- blocparty/src/main/java/com/bloc/blocparty/ui/activities/OnBoardActivity.java
+++ blocparty/src/main/java/com/bloc/blocparty/ui/activities/OnBoardActivity.java
@@ -3,15 +3,16 @@ package com.bloc.blocparty.ui.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.bloc.blocparty.R;
 import com.bloc.blocparty.ui.fragments.OnBoardFragment;
+import com.bloc.blocparty.ui.utils.Constants;
 
-public class OnBoardActivity extends FragmentActivity implements OnBoardFragment.OnFragmentInteractionListener {
+public class OnBoardActivity extends FragmentActivity implements OnBoardFragment.OnBoardingInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +21,7 @@ public class OnBoardActivity extends FragmentActivity implements OnBoardFragment
         setContentView(R.layout.activity_on_board);
 
         //checkIfAlreadyOnBoarded();
-
-        OnBoardFragment obFrag = new OnBoardFragment();
-        getFragmentManager().beginTransaction().replace(R.id.onBoardFrag, obFrag).commit();
+        loadOnBoardFrag(Constants.NETWORK_ID);
     }
 
      /*
@@ -42,8 +41,22 @@ public class OnBoardActivity extends FragmentActivity implements OnBoardFragment
         }
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
+    /*
+     * This mehtod loads the appropriate on boarding fragment as the user clicks through them and
+     * after they are all done, it loads the main activity
+     *
+     * @params: networkId The int id of which network is up for sign in
+     */
+    public void loadOnBoardFrag(int networkId) {
+        if(networkId == 3) {
+            Toast.makeText(this, "On-Boarding Complete!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, BlocParty.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            OnBoardFragment obFrag = new OnBoardFragment(this, networkId);
+            getFragmentManager().beginTransaction().replace(R.id.onBoardFrag, obFrag).commit();
+        }
     }
 }
--- blocparty/src/main/java/com/bloc/blocparty/ui/fragments/OnBoardFragment.java
+++ blocparty/src/main/java/com/bloc/blocparty/ui/fragments/OnBoardFragment.java
@@ -1,49 +1,95 @@
 package com.bloc.blocparty.ui.fragments;
 
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bloc.blocparty.R;
+import com.bloc.blocparty.ui.activities.OnBoardActivity;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link OnBoardFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
+ * This fragment allows the user to link one or more of their accounts during on boarding
  */
 public class OnBoardFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private int mNetworkId;
+    private Button mSignInButton;
+    private Context mContext;
 
-    public OnBoardFragment() {
-        // Required empty public constructor
+    public OnBoardFragment() {} // Required empty public constructor
+
+    public OnBoardFragment(Context context, int networkId) {
+        this.mContext = context;
+        this.mNetworkId = networkId;
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_board, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_on_board, container, false);
+
+        mSignInButton = (Button) rootView.findViewById(R.id.selectButton);
+        Button dismissButton = (Button) rootView.findViewById(R.id.dismissButton);
+        TextView title = (TextView) rootView.findViewById(R.id.networkTitle);
+        TextView message = (TextView) rootView.findViewById(R.id.networkMessage);
+        ImageView icon = (ImageView) rootView.findViewById(R.id.imageIcon);
+
+        if(mNetworkId == 0) {
+            mSignInButton.setTextColor(getResources().getColor(R.color.facebook_blue));
+            title.setText(getString(R.string.onboard_title_facebook));
+            message.setText(getString(R.string.onboard_message_facebook));
+            icon.setImageDrawable(getResources().getDrawable(R.drawable.ic_facebook_icon));
+            facebookOnBoard();
+        }
+        else if(mNetworkId == 1) {
+            mSignInButton.setTextColor(getResources().getColor(R.color.twitter_blue));
+            title.setText(getString(R.string.onboard_title_twitter));
+            message.setText(getString(R.string.onboard_message_twitter));
+            icon.setImageDrawable(getResources().getDrawable(R.drawable.ic_twitter_icon));
+            twitterOnBoard();
+        }
+        else if(mNetworkId == 2) {
+            mSignInButton.setTextColor(getResources().getColor(R.color.instagram_blue));
+            title.setText(getString(R.string.onboard_title_instagram));
+            message.setText(getString(R.string.onboard_message_instagram));
+            icon.setImageDrawable(getResources().getDrawable(R.drawable.ic_instagram_icon));
+            instagramOnBoard();
+        }
+
+        dismissButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mNetworkId++;
+                ((OnBoardActivity) mContext).loadOnBoardFrag(mNetworkId);
+            }
+        });
+
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    private void instagramOnBoard() {
+
+    }
+
+    private void twitterOnBoard() {
+
+    }
+
+    private void facebookOnBoard() {
+
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mListener = (OnFragmentInteractionListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -53,22 +99,10 @@ public class OnBoardFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    public interface OnBoardingInteractionListener {
+        public void loadOnBoardFrag(int networkId);
     }
 
 }
